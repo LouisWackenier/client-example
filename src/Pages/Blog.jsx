6697e8d41ffabb1865438e897c49852b4d1fe237
LouisWackenier <Louis.wackenier@gmail.com>
2023-09-15 09:45:27 +0200

    Set up the title and subtitle together with a breakline

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,13 +1,23 @@
 import styled from "styled-components";
 
 const ContainerTitle = styled.div`
-     margin-left: 37%;
+     display: block;
 `;
-const TitleSub = styled.div``;
 
 const Title = styled.h1`
     font-size: 30px;
-    
+    margin-left: 37%;
+`;
+
+const TitleSub = styled.h2`
+    margin-left: 32%;
+`;
+
+const BreakLine = styled.div`
+      margin-left: 31%;
+      width: 35%;
+      height: 2px; /* Set the height of the separate line */
+      background-color: rgba(255, 255, 255, 0.5); /* Color of the separate line */
 `;
 
 const MainContainer = styled.div`
@@ -20,6 +30,8 @@ const Blog = () => {
     <div>
       <ContainerTitle>
          <Title>Welcome to the Blog page</Title>
+         <TitleSub>On this page you can run your own blog posts</TitleSub>
+         <BreakLine />
       </ContainerTitle>
       <MainContainer>
            
