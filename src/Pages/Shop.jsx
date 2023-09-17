d9883c6a4e3eb8dd7c5bf563cf4af71f28950dd0
LouisWackenier <Louis.wackenier@gmail.com>
2023-09-17 09:16:04 +0200

    Built the shop title and sub title section

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,7 +1,34 @@
+import styled from 'styled-components'
+
+
+
+const ContainerTitle = styled.div`
+        
+`;
+
+const ContainerTitleText = styled.h1`
+      margin-left: 40%;
+`;
+const ContainerTitleSub = styled.h4`
+      margin-left: 38%;
+`;
+const Breakline = styled.div`
+      margin-left: 37%;
+      width: 25%;
+      height: 2px; /* Set the height of the separate line */
+      background-color: rgba(255, 255, 255, 0.5); /* Color of the separate line */
+`;
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <ContainerTitle>
+         <ContainerTitleText>Welcome to the shop</ContainerTitleText>
+         <ContainerTitleSub>
+               In this section you can add and sell your products
+         </ContainerTitleSub>
+         <Breakline />
+    </ContainerTitle>
+
   )
 }
 
